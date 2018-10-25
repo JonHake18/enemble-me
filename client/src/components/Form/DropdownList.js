7017cb08a2310ab2249fb5499e84449b1d838df3
unknown <jonhake18@gmail.com>
2018-10-24 20:40:54 -0500

    changes

--- client/src/components/Form/DropdownList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const DropdownList = props => 
-    <select className="custom-select" id={props.id}>{props.data.map((x,y) => <option key={y} value={x}>{x}</option>)}</select>;
-
-export default DropdownList;
-
--- models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-     User : require("./user.js"),
-     Options: require("./options.js"),
-     Musician : require("./musicians.js"),
-     Instrument : require("./instruments.js"),
-     Band: require("./bands.js")
-}
