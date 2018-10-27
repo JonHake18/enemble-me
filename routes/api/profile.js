2be11214415e553ba92f8e0052e652f2ecd15ec5
Yuendemu <matthew75kelly@gmail.com>
2018-10-27 06:21:42 -0500

    adding additional .env.production file to help maintain React environment variable with heroku deployment

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,17 +4,12 @@ const User = db.User;
 const router = require("express").Router();
 
 function sendProfileLoadAck(req, res) {
-<<<<<<< HEAD
-     User.findById(req.user._id)
-=======
     User.findById(req.user._id)
->>>>>>> 2d9ecb57d3772b25d6e5c7b63959918c8665ffd4
     .populate("instrumentsPlayed")
     .populate("instrumentsDesired")
     .then(user=>{
       res.status(200).json({
       message: "You're authorized to see this secret message.",
-<<<<<<< HEAD
       // user values passed through from auth middleware	  
       user: user
       })
@@ -22,12 +17,6 @@ function sendProfileLoadAck(req, res) {
     .catch(err=>{
       console.log(`Could not retrieve document with matching id:\n\t${err}`);
     })    
-=======
-      // user values passed through from auth middleware
-      user: user
-    });
-    })
->>>>>>> 2d9ecb57d3772b25d6e5c7b63959918c8665ffd4
 }
 router.route('/')
   .get(sendProfileLoadAck);
